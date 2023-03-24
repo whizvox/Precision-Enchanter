947c1d8632a9cdce2fb6a65abd1f78714a402c7c
Corneilious Eanes <corneiliouseanes@gmail.com>
2023-03-24 00:44:07 -0700

    Finalize 1.0.0 - Add recipes for Block Strength and Preservation (Twilight Forest) - Nerf Destruction enchantment recipe (Twilight Forest)

--- src/main/java/me/whizvox/precisionenchanter/data/server/compat/TwilightForestEnchantmentRecipeProvider.java
+++ src/main/java/me/whizvox/precisionenchanter/data/server/compat/TwilightForestEnchantmentRecipeProvider.java
@@ -5,9 +5,14 @@ import me.whizvox.precisionenchanter.common.recipe.ConditionalEnchantmentRecipe;
 import me.whizvox.precisionenchanter.common.recipe.EnchantmentRecipe;
 import me.whizvox.precisionenchanter.data.server.EnchantmentRecipeProvider;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.registries.ForgeRegistries;
 import twilightforest.TwilightForestMod;
+import twilightforest.block.TFBlocks;
 import twilightforest.enchantment.TFEnchantments;
 import twilightforest.item.TFItems;
 
@@ -79,28 +84,52 @@ public class TwilightForestEnchantmentRecipeProvider extends EnchantmentRecipePr
         .build());
 
     // Destruction
+    final Item KNIGHTMETAL_BLOCK = ForgeRegistries.ITEMS.getValue(TFBlocks.KNIGHTMETAL_BLOCK.getId());
     output.accept(builder(TFEnchantments.DESTRUCTION.get(), 1)
-        .ingredient(Items.IRON_BLOCK)
-        .ingredient(Items.WOODEN_PICKAXE)
-        .ingredient(TFItems.KNIGHTMETAL_INGOT.get(), 5)
-        .cost(1)
-        .build());
-
-    output.accept(builder(TFEnchantments.DESTRUCTION.get(), 2)
-        .ingredient(Items.IRON_BLOCK)
-        .ingredient(Items.STONE_PICKAXE)
-        .ingredient(TFItems.KNIGHTMETAL_INGOT.get(), 10)
+        .ingredient(KNIGHTMETAL_BLOCK)
+        .ingredient(Items.IRON_SHOVEL)
         .cost(3)
         .build());
 
-    output.accept(builder(TFEnchantments.DESTRUCTION.get(), 3)
-        .ingredient(Items.IRON_BLOCK)
-        .ingredient(Items.IRON_PICKAXE)
-        .ingredient(TFItems.KNIGHTMETAL_INGOT.get(), 20)
-        .cost(5)
+    output.accept(builder(TFEnchantments.DESTRUCTION.get(), 2)
+        .ingredient(KNIGHTMETAL_BLOCK)
+        .ingredient(Items.DIAMOND_SHOVEL)
+        .cost(6)
         .build());
 
-    // TODO Add recipes for Preservation and Block Strength
+    output.accept(builder(TFEnchantments.DESTRUCTION.get(), 3)
+        .ingredient(KNIGHTMETAL_BLOCK)
+        .ingredient(Items.NETHERITE_SHOVEL)
+        .cost(10)
+        .build());
+
+    // Block Strength
+    output.accept(builder(TFEnchantments.BLOCK_STRENGTH.get(), 1)
+        .ingredient(KNIGHTMETAL_BLOCK)
+        .ingredient(TFItems.KNIGHTMETAL_PICKAXE.get())
+        .ingredient(Tags.Items.INGOTS_IRON, 3)
+        .cost(2)
+        .build());
+    output.accept(builder(TFEnchantments.BLOCK_STRENGTH.get(), 2)
+        .ingredient(KNIGHTMETAL_BLOCK)
+        .ingredient(TFItems.KNIGHTMETAL_PICKAXE.get())
+        .ingredient(Tags.Items.GEMS_DIAMOND, 3)
+        .cost(4)
+        .build());
+    output.accept(builder(TFEnchantments.BLOCK_STRENGTH.get(), 3)
+        .ingredient(KNIGHTMETAL_BLOCK)
+        .ingredient(TFItems.KNIGHTMETAL_PICKAXE.get())
+        .ingredient(Tags.Items.INGOTS_NETHERITE)
+        .cost(7)
+        .build());
+
+    // Preservation
+    output.accept(builder(TFEnchantments.PRESERVATION.get())
+        .ingredient(KNIGHTMETAL_BLOCK)
+        .ingredient(ItemTags.WOOL, 32)
+        .ingredient(Items.SLIME_BLOCK, 12)
+        .cost(4)
+        .build());
 
   }
 
